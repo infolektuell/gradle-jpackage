1b993a2d684090f6c8c032e5f7326a2f0b5c0efd
Tamara Cook <info@infolektuell.de>
2025-11-29 14:31:18 +0100

    Add example app

---
+++ example/app/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    // Apply the application plugin to add support for building a CLI application in Java.
+    application
+    id("de.infolektuell.jpackage")
+}
+
+jpackage {
+runtime {
+    modules = listOf("java.base")
+}
+    argFiles = listOf(layout.projectDirectory.file("src/args.txt"))
+}
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+    // Use JUnit Jupiter for testing.
+    testImplementation(libs.junit.jupiter)
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // This dependency is used by the application.
+    implementation(libs.guava)
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+application {
+    // Define the main class for the application.
+    mainClass = "org.example.App"
+}
+
+tasks.named<Test>("test") {
+    // Use JUnit Platform for unit tests.
+    useJUnitPlatform()
+}
