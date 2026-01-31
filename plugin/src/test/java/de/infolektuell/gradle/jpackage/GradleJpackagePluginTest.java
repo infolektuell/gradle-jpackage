c7c2d0b0cd37adb0bf4b6ae324df3a2669644795
Tamara Cook <info@infolektuell.de>
2026-01-31 23:13:27 +0100

    Add more unit tests

--- plugin/src/test/java/de/infolektuell/gradle/jpackage/GradleJpackagePluginTest.java
+++ plugin/src/test/java/de/infolektuell/gradle/jpackage/GradleJpackagePluginTest.java
@@ -6,12 +6,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GradleJpackagePluginTest {
-    @Test void pluginRegistersATask() {
-        // Create a test project and apply the plugin
+    @Test void pluginApliesSucessfully() {
         Project project = ProjectBuilder.builder().build();
         project.getPlugins().apply(GradleJpackagePlugin.PLUGIN_NAME);
-
-        // Verify the result
         assertNotNull(project.getExtensions().findByName("jpackage"));
+        assertNull(project.getTasks().findByName("appImage"));
+    }
+
+    @Test void pluginRegistersAppImageTask() {
+        Project project = ProjectBuilder.builder().build();
+        project.getPlugins().apply("application");
+        project.getPlugins().apply(GradleJpackagePlugin.PLUGIN_NAME);
+        assertNotNull(project.getTasks().findByName("appImage"));
     }
 }
