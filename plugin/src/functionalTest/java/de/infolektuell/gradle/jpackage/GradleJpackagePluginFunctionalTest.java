5fc1e8e27eb5fac85ecfc7071046332e59ea18db
Tamara Cook <info@infolektuell.de>
2026-02-01 17:28:44 +0100

    Add more functional tests

--- plugin/src/functionalTest/java/de/infolektuell/gradle/jpackage/GradleJpackagePluginFunctionalTest.java
+++ plugin/src/functionalTest/java/de/infolektuell/gradle/jpackage/GradleJpackagePluginFunctionalTest.java
@@ -1,34 +1,111 @@
 package de.infolektuell.gradle.jpackage;
 
-import java.io.File;
-
 import org.gradle.testkit.runner.GradleRunner;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GradleJpackagePluginFunctionalTest {
     private final File projectDir = new File("../example");
+    private final String gradleVersion = "9.1.0";
+
     @Test
-    void nonModularProject() {
-        var runner = createRunner("nonmodular", "9.1.0");
+    void nonModularProjectShouldBuild() {
+        final var project = "nonmodular";
+        var runner = createRunner()
+            .withArguments(project + ":clean", project + ":build", "--stacktrace");
         var result = runner.build();
         assertTrue(result.getOutput().contains("BUILD SUCCESSFUL"));
     }
 
     @Test
-    void modularProject() {
-        var runner = createRunner("modular", "9.1.0");
+    void modularProjectShouldBuild() {
+        final var project = "modular";
+        var runner = createRunner()
+            .withArguments(project + ":clean", project + ":build", "--stacktrace");
         var result = runner.build();
         assertTrue(result.getOutput().contains("BUILD SUCCESSFUL"));
     }
 
-    private GradleRunner createRunner(String project, String gradleVersion) {
+    @Test
+    void nonModularProjectShouldFindModuleDeps() {
+        final var project = "nonmodular";
+        var runner = createRunner()
+            .withArguments(project + ":clean", project + ":findModuleDeps", "--stacktrace");
+        var result = runner.build();
+        assertTrue(result.getOutput().contains("BUILD SUCCESSFUL"));
+    }
+
+    @Test
+    void modularProjectShouldFindModuleDeps() {
+        final var project = "modular";
+        var runner = createRunner()
+            .withArguments(project + ":clean", project + ":findModuleDeps", "--stacktrace");
+        var result = runner.build();
+        assertTrue(result.getOutput().contains("BUILD SUCCESSFUL"));
+    }
+
+    @Test
+    void nonModularProjectShouldGenerateRuntimeImage() {
+        final var project = "nonmodular";
+        var runner = createRunner()
+            .withArguments(project + ":clean", project + ":generateRuntimeImage", "--stacktrace");
+        var result = runner.build();
+        assertTrue(result.getOutput().contains("BUILD SUCCESSFUL"));
+    }
+
+    @Test
+    void modularProjectShouldGenerateRuntimeImage() {
+        final var project = "modular";
+        var runner = createRunner()
+            .withArguments(project + ":clean", project + ":generateRuntimeImage", "--stacktrace");
+        var result = runner.build();
+        assertTrue(result.getOutput().contains("BUILD SUCCESSFUL"));
+    }
+
+    @Test
+    void nonModularProjectShouldgenerateAppImage() {
+        final var project = "nonmodular";
+        var runner = createRunner()
+            .withArguments(project + ":clean", project + ":appImage", "--stacktrace");
+        var result = runner.build();
+        assertTrue(result.getOutput().contains("BUILD SUCCESSFUL"));
+    }
+
+    @Test
+    void modularProjectShouldgenerateAppImage() {
+        final var project = "modular";
+        var runner = createRunner()
+            .withArguments(project + ":clean", project + ":appImage", "--stacktrace");
+        var result = runner.build();
+        assertTrue(result.getOutput().contains("BUILD SUCCESSFUL"));
+    }
+
+    @Test
+    void nonModularAppShouldRun() {
+        final var project = "nonmodular";
+        var runner = createRunner()
+            .withArguments(project + ":clean", project + ":run", "--stacktrace");
+        var result = runner.build();
+        assertTrue(result.getOutput().contains("BUILD SUCCESSFUL"));
+    }
+
+    @Test
+    void modularAppShouldRun() {
+        final var project = "modular";
+        var runner = createRunner()
+            .withArguments(project + ":clean", project + ":run", "--stacktrace");
+        var result = runner.build();
+        assertTrue(result.getOutput().contains("BUILD SUCCESSFUL"));
+    }
+
+    private GradleRunner createRunner() {
         return GradleRunner.create()
+            .forwardOutput()
             .withPluginClasspath()
             .withProjectDir(projectDir)
-            .withGradleVersion(gradleVersion)
-            .withArguments(project + ":clean", project + ":build", "--stacktrace")
-            .forwardOutput();
+            .withGradleVersion(gradleVersion);
     }
 }
